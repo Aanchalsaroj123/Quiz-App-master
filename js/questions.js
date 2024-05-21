a62abf12bc5f677c79e986cf622a216350f3054a
Aanchalsaroj123 <aanchal.saroj.ug21@nsut.ac.in>
2024-05-21 19:40:33 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,95 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Which of the following sports facilities is not available at NSUT?",
+    answer: "D) Swimming Pool",
+    options: [
+      "A) Basketball Court",
+      "B) Tennis Court",
+      "C) Cricket Ground",
+      "D) Swimming Pool",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which university is NSUT affiliated with?",
+    answer: "A) University of Delhi",
+    options: [
+      "A) University of Delhi",
+      "B) Delhi Technological University (DTU)",
+      "C) Jamia Millia Islamia (JMI)",
+      "D) Jawaharlal Nehru Technological University (JNTU)",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Which one of them is a Tech society of NSUT?",
+    answer: "D) IEEE",
+    options: ["A) Crosslinks", "B) GDSC", "C) IGTS", "D) IEEE"],
+  },
+  {
+    numb: 4,
+    question:
+      "What year was NSUT established?",
+    answer: "C) 1983",
+    options: ["A) 1997", "B) 1999", "C) 1983", "D) 1984"],
+  },
+  {
+    numb: 5,
+    question: "Which campus is NSUT located in?",
+    answer: "A) Dwarka",
+    options: ["A) Dwarka", "B) Rohini", "C) Janakpuri", "D) Saket"],
+  },
+  {
+    numb: 6,
+    question: "What does NSS stand for at NSUT?",
+    answer: "A) National Service Scheme",
+    options: [
+      "A) National Service Scheme",
+      "B) National Science Society",
+      "C) National Student Society",
+      "D) National Sports Society",
+    ],
+  },
+  {
+    numb: 7,
+    question:
+      "Who is the founder of NSUT?",
+    answer: "C) Prof. Jai Prakash Saini",
+    options: [
+      "A) Brahm Prakash",
+      "B) Sh. Atal Bihari Vajpayee",
+      "C) Prof. Jai Prakash Saini",
+      "D) Maharaja Agrasen",
+    ],
+  },
+  {
+    numb: 8,
+    question: "Which of the following is not a course offered at NSUT?",
+    answer: "D) Bioinformatics Engineering",
+    options: [
+      "A) Computer Science and Engineering",
+      "B) Electronics and Communication Engineering",
+      "C) Mechanical Engineering",
+      "D) Bioinformatics Engineering",
+    ],
+  },
+  {
+    numb: 9,
+    question: "What is the name of the annual technical fest at NSUT?",
+    answer: "D) MOKSHA",
+    options: [
+      "A) Technotsav",
+      "B) Innovision",
+      "C) Technovision",
+      "D) MOKSHA",
+    ],
+  },
+  {
+    numb: 10,
+    question: "Which one of them is a Gming society of NSUT?",
+    answer: "C) IGTS",
+    options: ["A) Crosslinks", "B) GDSC", "C) IGTS", "D) IEEE"],
+  },
+];
